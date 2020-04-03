eddbc0982e43be971f7a7dd591f9d4ea5f8e2cdf
Stanley A. Baronett <stanley.a.baronett@gmail.com>
2020-04-03 15:06:17 -0700

    Update docstring

--- nr/03interp/c/spline_reader.py
+++ nr/03interp/c/spline_reader.py
@@ -1,19 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def splineplot(fignum, bctype, xa, ya, x, y_int):
+def splineplot(fignum, dset, bctype, xa, ya, x, y_int):
     """
     Matplotlib script to plot dataset and interpolation.
+
+    Parameters
+    ----------
+    fignum : integer, optional, default: None
+        Figure number to be displayed on plot.
+
+    dset : integer, optional, default: None
+        Dataset number to be displayed on plot.
+
+    bctype : string, optional, default: None
+        Boundary condition type to be displayed on plot.
     """
     plt.figure(figsize=(5,5))
-    plt.title(r'Figure %d. $Dataset \hspace{0.5}\#%d$'%(fignum, fignum),
+    plt.title(r'Figure %d. $Dataset \hspace{0.5}\#%d$'%(fignum, dset),
               fontsize='xx-large', fontweight='bold')
     plt.xlabel(r'$x$', fontsize='large')
     plt.ylabel(r'$y$', fontsize='large')
     plt.axvline(color='k')
     plt.axhline(color='k')
     plt.plot(xa, ya, 'o', label='$y_i(x_i)$')
-    plt.plot(x, y_int, label='Cubic spline')
+    plt.plot(x, y_int, label='Cubic spline (%s)'%(bctype))
     plt.legend(loc='best', fontsize='x-large')
     plt.grid()
     plt.tight_layout()
@@ -22,7 +33,7 @@ def splineplot(fignum, bctype, xa, ya, x, y_int):
 def read(filename):
     f = open(filename, 'r')
     data = []
-    x = np.array([])
+    x = np.aray([])
     y = np.array([])
     for line in f:
         data.append(line)
